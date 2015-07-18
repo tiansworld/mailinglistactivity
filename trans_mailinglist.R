9fed01527f7aa1941189769e93d1efd40769ecfc
Tian Shixiong <tiansworld@fedoraproject.org>
2015-07-18 10:48:26 +0800

    Correct data frame building code, add title, labs to the figure.

--- trans_mailinglist.R
+++ trans_mailinglist.R
@@ -3,6 +3,7 @@ library(RCurl)
 library(htmltools)
 library(zoo)
 library(ggplot2)
+library(lubridate)
 
 
 ## Get the mailing list archieve page url
@@ -18,8 +19,8 @@ finaltrans <- xpathApply(htmlTreeParse(transhtml,useInternalNodes = T),"//td",
 
 ## Set the list index that the content will be extracted. Here extract the 
 ## archieve month and year, and the size of the archieve.
-filtermonth <- seq(4,423,3)
-filtersize <- seq(6,423,3)
+filtermonth <- seq(4,length(finaltrans),3)
+filtersize <- seq(6,length(finaltrans),3)
 
 ## Extract the month, year and archieve size, then change the extracted lists to 
 ## vectors
@@ -32,11 +33,14 @@ month <- as.character(sapply(month, function(i) gsub(":","",i)))
 size <- as.numeric(sapply(size,function(i) gsub("[^0-9]+","",i)))
 
 ## Built a data frame contains archieve year, month and size
-archieveyear <- as.factor(year(as.yearmon(trans$month)))
-archievemonth <- as.factor(month(as.yearmon(trans$month)))
-trans <- data.frame(year=archieveyear,month=archievemonth,size=trans$size)
+archieveyear <- as.factor(year(as.yearmon(month)))
+archievemonth <- as.factor(month(as.yearmon(month)))
+trans <- data.frame(year=archieveyear,month=archievemonth,size=size)
 
 ## Plot
 fig <- ggplot(trans) + aes(x=month,y=size,group=year,color=year) +
-        facet_wrap(~ year) + geom_line(aes(size=size))
+        geom_line(size=2) + labs(title="Activity of translation mailing list",x = "Month",y = "Archive Size (KB)")
 ggsave("trans_archieve.png",fig)
+fig <- fig + facet_wrap(~ year)
+ggsave("wrap.png",fig)
+dev.off()
